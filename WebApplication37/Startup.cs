287e7754db4caaaec1a1ec69c7595ae3f837ec9d
explorer14 <amanag1983@gmail.com>
2021-01-18 21:27:52 +0100

    Add reference to the infra project

--- WebApplication37/Startup.cs
+++ WebApplication37/Startup.cs
@@ -1,12 +1,14 @@
 using AppPerformanceMetricsSender.Extensions;
 using AppPerformanceMetricsSender.PerformanceMetrics;
 using AppPerformanceMetricsSender.Publishing;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System.Reflection;
 
 namespace WebApplication37
 {
@@ -30,8 +32,7 @@ namespace WebApplication37
                 options: new PerfMetricsSenderOptions
                 {
                     MetricCollectionInterval = 2000
-                },
-                tags: new MetricTag("environment", "development"));
+                }, assemblyToLoadAdditionalMetricsFrom: Assembly.GetAssembly(typeof(DummyMetric2)));
 
 
             //services.AddPerfMetricSender(
