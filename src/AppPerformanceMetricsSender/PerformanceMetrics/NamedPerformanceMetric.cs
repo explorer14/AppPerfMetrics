dec8a73e9a7e8d2886d8ea77ac67b1316a6192db
explorer14 <amanag1983@gmail.com>
2021-01-18 16:38:56 +0100

    Add documentation to the base type

--- src/AppPerformanceMetricsSender/PerformanceMetrics/NamedPerformanceMetric.cs
+++ src/AppPerformanceMetricsSender/PerformanceMetrics/NamedPerformanceMetric.cs
@@ -4,10 +4,19 @@ using System.Linq;
 
 namespace AppPerformanceMetricsSender.PerformanceMetrics
 {
+    /// <summary>
+    /// Base class for performance metrics. Inherit from this class to define
+    /// custom metrics for your app.
+    /// </summary>
     public abstract class NamedPerformanceMetric : IPerformanceMetric
     {
         private readonly string tags;
 
+        /// <summary>
+        /// Instantiate the base class
+        /// </summary>
+        /// <param name="appGroup">Identifier for the app</param>
+        /// <param name="tags">Custom tags i.e. name value pairs to associate with this metric</param>
         protected NamedPerformanceMetric(string appGroup, params MetricTag[] tags)
         {
             if (string.IsNullOrWhiteSpace(appGroup))
@@ -20,10 +29,32 @@ namespace AppPerformanceMetricsSender.PerformanceMetrics
             Tags = tags;
         }
 
+        /// <summary>
+        /// The value of this metric. For time based metrics prefer [milliseconds] to give yourself
+        /// more fine grained value, for space based metrics prefer [bytes]. Any conversion to coarser
+        /// grained units like MB, seconds, minutes etc should be done on the monitoring platform
+        /// </summary>
         public abstract long Count { get; }
+
+        /// <summary>
+        /// Used by the publisher to fully identify the metric
+        /// </summary>
         public string FullyQualifiedName => $"{AppGroup}.{Name}";
+
+        /// <summary>
+        /// Name for the metric. Will be lower cased and spaces removed
+        /// </summary>
         public abstract string Name { get; }
+
+        /// <summary>
+        /// Tags to associate with the metric for e.g. environment, 
+        /// country etc.
+        /// </summary>
         public MetricTag[] Tags { get; }
+
+        /// <summary>
+        /// App identifier string for e.g. "finance-api"
+        /// </summary>
         protected string AppGroup { get; }
 
         public override string ToString()
