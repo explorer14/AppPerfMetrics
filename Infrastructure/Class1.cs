75e9c0f61325dc9d1819997479563aee7423d6d3
explorer14 <amanag1983@gmail.com>
2021-01-18 21:27:34 +0100

    Add a sample project to demo loading from a different assembly than host

---
+++ Infrastructure/Class1.cs
@@ -0,0 +1,17 @@
+﻿using AppPerformanceMetricsSender.PerformanceMetrics;
+using AppPerformanceMetricsSender.Publishing;
+
+namespace Infrastructure
+{
+    public class DummyMetric2 : NamedPerformanceMetric
+    {
+        public DummyMetric2(string appGroup, params MetricTag[] tags)
+            : base(appGroup, tags)
+        {
+        }
+
+        public override long Count => 777;
+
+        public override string Name => "dummy";
+    }
+}
