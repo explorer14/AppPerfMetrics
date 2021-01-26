08e5bc5ea927e3c9f54f073787325a81ea06222e
explorer14 <amanag1983@gmail.com>
2021-01-26 21:23:53 +0100

    Add WorkingSet memory metric

---
+++ src/AppPerformanceMetricsSender/PerformanceMetrics/Memory/ProcessWorkingSetMemoryInBytes.cs
@@ -0,0 +1,17 @@
+﻿using AppPerformanceMetricsSender.Publishing;
+using System.Diagnostics;
+
+namespace AppPerformanceMetricsSender.PerformanceMetrics.Memory
+{
+    internal class ProcessWorkingSetMemoryInBytes : NamedPerformanceMetric
+    {
+        public ProcessWorkingSetMemoryInBytes(string appGroup, params MetricTag[] tags) 
+            : base(appGroup, tags)
+        {
+        }
+
+        public override long Value => Process.GetCurrentProcess().WorkingSet64;
+
+        public override string Name => "processworkingsetmemorybytes";
+    }
+}
